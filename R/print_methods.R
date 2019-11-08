434270b845eccb935b16958ccbeea9948cef45e4
Mark Edmondson <github@markedmondson.me>
2019-11-08 16:36:12 +0100

    add build status and wait

--- R/print_methods.R
+++ R/print_methods.R
@@ -5,8 +5,18 @@ print.ServiceList <- function(x, ...){
 
 #' @export
 print.BuildOperationMetadata <- function(x, ...){
-   cat("==BuildOperationMetadata==")
+   cat("==CloudBuildOperationMetadata==")
    cat("\nbuildId: ", x$metadata$build$id)
    cat("\nstatus: ", x$metadata$build$status)
    cat("\nlogUrl: ", x$metadata$build$logUrl)
+}
+
+#' @export
+print.gar_Build <- function(x, ...){
+  cat("==CloudBuildObject==")
+  cat("\nbuildId: ", x$id)
+  cat("\nstatus: ", x$status)
+  cat("\nlogUrl: ", x$logUrl)
+  cat("\nsteps: \n")
+  print(x$steps)
 }
