cee4c58391a354239d063254d6ddb492896c2afa
Mark Edmondson <github@markedmondson.me>
2019-11-20 22:43:09 +0100

    docs and update cr_scheduler to allow overwrite

--- R/cloudscheduler.R
+++ R/cloudscheduler.R
@@ -1,10 +1,11 @@
-#' Creates a Cloud Scheduler job.
+#' Creates or updates a Cloud Scheduler job.
 #'
 #' @seealso \href{https://cloud.google.com/scheduler/docs/reference/rest/v1/projects.locations.jobs/create}{Google Documentation for Cloud Scheduler}
 #'
 #' @inheritParams Job
-#' @param projectId The GCP project to run within
-#' @param region The region
+#' @param projectId The GCP project to run within usually set with \link{cr_project_set}
+#' @param region The region usually set with \link{cr_region_set}
+#' @param overwrite If TRUE and an existing job with the same name exists, will overwrite it with the new parameters
 #'
 #' @importFrom googleAuthR gar_api_generator
 #' @family Cloud Scheduler functions
@@ -12,23 +13,27 @@
 #' @examples
 #'
 #' \dontrun{
-#' cr_schedule("* * * * *", name="test", httpTarget = HttpTarget(uri="https://code.markedmondson.me"))
+#' cr_schedule("test", "* * * * *", httpTarget = HttpTarget(uri="https://code.markedmondson.me"))
 #'
 #' # schedule a cloud build (no source)
 #' build1 <- cr_build_make("cloudbuild.yaml")
-#' cr_schedule("15 5 * * *", name="cloud-build-test",
+#' cr_schedule("cloud-build-test", "15 5 * * *",
 #'              httpTarget = cr_build_schedule_http(build1))
 #'
 #' # schedule a cloud build with code source from GCS bucket
 #' my_gcs_source <- cr_build_upload_gcs("my_folder", bucket = cr_get_bucket())
 #' build <- cr_build_make("cloudbuild.yaml", source = my_gcs_source))
-#' cr_schedule("15 5 * * *", name="cloud-build-test",
+#' cr_schedule("cloud-build-test2", "15 5 * * *",
 #'             httpTarget = cr_build_schedule_http(build))
+#'
+#' # update a schedule with the same name - only supply what you want to change
+#' cr_schedule("cloud-build-test2", "12 6 * * *", overwrite=TRUE)
 #' }
-cr_schedule <- function(schedule,
-                        name=NULL,
+cr_schedule <- function(name,
+                        schedule=NULL,
                         httpTarget=NULL,
                         description=NULL,
+                        overwrite=FALSE,
                         region = cr_region_get(),
                         projectId = cr_project_get()
                         ) {
@@ -38,22 +43,69 @@ cr_schedule <- function(schedule,
     is.string(schedule)
   )
 
-  the_url <- sprintf("https://cloudscheduler.googleapis.com/v1/projects/%s/locations/%s/jobs",
-                 projectId, region)
-
+  the_name <- sprintf("projects/%s/locations/%s/jobs/%s", projectId, region, name)
   job <- Job(schedule=schedule,
-             name = sprintf("projects/%s/locations/%s/jobs/%s", projectId, region, name),
+             name = the_name,
              httpTarget = httpTarget,
              description = description)
 
-  # cloudscheduler.projects.locations.jobs.create
-  f <- gar_api_generator(the_url, "POST",
-                         data_parse_function = function(x) structure(x,
-                                                                     class = "gar_scheduleJob"))
+  if(overwrite){
+    scheds <- cr_schedule_list(region = region, projectId = projectId)
+    if(the_name %in% scheds$name){
+      myMessage("Overwriting schedule job: ", name, level=3)
+      # https://cloud.google.com/scheduler/docs/reference/rest/v1/projects.locations.jobs/patch
+      the_url <-
+        sprintf("https://cloudscheduler.googleapis.com/v1/projects/%s/locations/%s/jobs/%s",
+                projectId, region, name)
+      updateMask <- rmNullObs(list(schedule = schedule,
+                             httpTarget = httpTarget,
+                             description = description))
+
+      f <- gar_api_generator(the_url, "PATCH",
+              data_parse_function = parse_schedule,
+              pars_args = list(updateMask = paste(names(updateMask), collapse = ",")))
+    }
+  } else {
+    the_url <-
+      sprintf("https://cloudscheduler.googleapis.com/v1/projects/%s/locations/%s/jobs",
+              projectId, region)
+    # cloudscheduler.projects.locations.jobs.create
+    f <- gar_api_generator(the_url, "POST",
+                           data_parse_function = parse_schedule)
+
+  }
+
   stopifnot(inherits(job, "gar_scheduleJob"))
 
-  f(the_body = job)
+  o <- f(the_body = job)
 
+  # check for update operations
+  if(o$state == "UPDATE_FAILED"){
+    o <- retry_update_failed(job, f)
+  }
+
+  o
+
+}
+
+parse_schedule <- function(x){
+  structure(x,
+            class = "gar_scheduleJob")
+}
+
+retry_update_failed <- function(job, f) {
+  myMessage("Update failed status, retrying", level = 3)
+  i <- 0
+  while(i < as.integer(getOption("googleAuthR.tryAttempts"))){
+    o <- f(the_body = job)
+    if(o$state != "UPDATE_FAILED"){
+      myMessage("Retry successful", level = 3)
+      return(o)
+    }
+    i <- i + 1
+  }
+  myMessage("Retry unsuccessful", level = 3)
+  o
 }
 
 #' Lists Cloud Scheduler jobs.
