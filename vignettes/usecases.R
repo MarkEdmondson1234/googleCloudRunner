d251035fd6fb5ea0a3ef0b8ee3a8ee324cbb15d4
Mark Edmondson <github@markedmondson.me>
2019-12-31 01:08:01 +0100

    comb through documentation, add RStudio gadget

---
+++ vignettes/usecases.R
@@ -0,0 +1,72 @@
+## ---- code=readLines("polygot.R"), eval=FALSE, echo = TRUE--------------------
+#  library(googleCloudRunner)
+#  polygot <- cr_build_yaml(
+#    steps = c(
+#      cr_buildstep(
+#        id = "download encrypted auth file",
+#        name = "gsutil",
+#        args = c("cp",
+#                 "gs://marks-bucket-of-stuff/auth.json.enc",
+#                 "/workspace/auth.json.enc"),
+#      ),
+#      cr_buildstep_decrypt(
+#        id = "decrypt file",
+#        cipher = "/workspace/auth.json.enc",
+#        plain = "/workspace/auth.json",
+#        keyring = "my-keyring",
+#        key = "ga_auth"
+#      ),
+#      cr_buildstep(
+#        id = "download google analytics",
+#        name = "gcr.io/gcer-public/gago:master",
+#        env = c("GAGO_AUTH=/workspace/auth.json"),
+#        args = c("reports",
+#                 "--view=81416156",
+#                 "--dims=ga:date,ga:medium",
+#                 "--mets=ga:sessions",
+#                 "--start=2014-01-01",
+#                 "--end=2019-11-30",
+#                 "--antisample",
+#                 "--max=-1",
+#                 "-o=google_analytics.csv"),
+#        dir = "build"
+#      ),
+#      cr_buildstep(
+#        id = "download Rmd template",
+#        name = "gsutil",
+#        args = c("cp",
+#                 "gs://mark-edmondson-public-read/polygot.Rmd",
+#                 "/workspace/build/polygot.Rmd")
+#      ),
+#      cr_buildstep_r(
+#        id="render rmd",
+#        r = "lapply(list.files('.', pattern = '.Rmd', full.names = TRUE),
+#               rmarkdown::render, output_format = 'html_document')",
+#        name = "gcr.io/gcer-public/packagetools:master",
+#        dir = "build"
+#        ),
+#      cr_buildstep_bash(
+#        id = "setup nginx",
+#        bash_script = system.file("docker", "nginx", "setup.bash",
+#                                  package = "googleCloudRunner"),
+#        dir = "build"
+#        ),
+#      cr_buildstep_docker(
+#        # change to your own container registry
+#        image = "gcr.io/gcer-public/polygot_demo",
+#        tag = "latest",
+#        dir = "build"
+#        ),
+#      cr_buildstep_run(
+#        name = "polygot-demo",
+#        image = "gcr.io/gcer-public/polygot_demo",
+#        concurrency = 80)
+#    )
+#  )
+#  
+#  # build the polygot cloud build steps
+#  build <- cr_build(polygot)
+#  built <- cr_build_wait(build)
+#  
+#  
+
