8a2e7211c7e56b47c173ed4c413bcf5d0f76c577
Mark Edmondson <github@markedmondson.me>
2020-04-23 22:22:34 +0200

    An auth setup function using googleAuthR 1.2 #53

--- R/setup.R
+++ R/setup.R
@@ -195,7 +195,7 @@ get_bucket_setup <- function(){
 
 get_auth_setup <- function(){
 
-  auth <- usethis::ui_yeah("Have you created and downloaded a JSON service account auth key?")
+  auth <- cr_setup_auth()
 
   if(auth){
     cli_ul("Browse to the file to be used for authentication")
@@ -216,7 +216,7 @@ get_auth_setup <- function(){
 
   }
 
-  cli_ul("Create a JSON service account auth key in your GCP project")
+  cli_ul("Run cr_setup_auth()")
 
   cli_ul("See website https://code.markedmondson.me/googleCloudRunner/articles/setup.html#local-auth-email")
 
@@ -255,4 +255,115 @@ do_env_check <- function(env_arg,
   NULL
 }
 
+#' Create a service account for googleCloudRunner
+#'
+#' This will use your Google OAuth2 user to create a suitable service account
+#'
+#' @param email What email to open OAuth2 with
+#' @param file Where to save the authentication file
+#' @param accountId Name of the service account in GCP IAM
+#'
+#' @return TRUE if the file is ready to be setup by \link{cr_setup}
+#'
+#' @export
+#' @importFrom googleAuthR gar_service_provision
+#' @import cli
+cr_setup_auth <- function(email = Sys.getenv("GARGLE_EMAIL"),
+                          file = "googlecloudrunner-auth-key.json",
+                          accountId = "googlecloudrunner"){
+
+  if(Sys.getenv("GAR_CLIENT_JSON") == ""){
+    cli_alert_info("Could not find a OAuth 2.0 Client ID via GAR_CLIENT_JSON")
+
+    client_id <- usethis::ui_yeah("Have you downloaded a JSON Client ID from your GCP?")
+
+    if(!client_id){
+      cli_alert_info("Download via https://console.cloud.google.com/apis/credentials")
+      cli_ul(c("Sidebar Menu >",
+                    "APIs & Services >",
+                    "Credentials >",
+                    "+ Create Credentials",
+                    "OAuth client ID >",
+                    "Other >",
+                    "Download to your PC"))
+      cli_alert_info("Rerun this wizard once you have your Client ID file")
+      return(FALSE)
+    }
+
+    cli_alert_info("Where is your client ID file?")
+    json <- file.choose()
+    valid <- validate_json(json)
+
+    if(valid){
+      cli_alert_success(
+          "Validated Client ID file {json} for project: {validated$installed$project_id}")
+      cli_ul("Include this code in your .Renviron to set client ID for all future R sessions")
+      cli_code(paste0("GAR_CLIENT_JSON=",json))
+      usethis::edit_r_environ()
+        cli_ul("Rerun this wizard once .Renviron is updated and R restarted")
+    }
+    # we always return that cr_setup() needs to be rerun
+    return(FALSE)
+
+  }
+
+  cli_alert_info("Using Client ID via GAR_CLIENT_JSON")
+  json <- Sys.getenv("GAR_CLIENT_JSON")
+  if(!validate_json(json)){
+    return(FALSE)
+  }
+
+  create_service <- usethis::ui_yeah("Do you want to provision a service account?")
+
+  if(!create_service){
+    cli_alert_danger("No service account provisioned")
+    return(TRUE)
+  }
+
+  roles <- c("roles/cloudbuild.builds.builder",
+             "roles/secretmanager.secretAccessor",
+             "roles/cloudscheduler.admin",
+             "roles/iam.serviceAccountUser",
+             "roles/run.admin",
+             "roles/storage.admin")
+
+  cli_alert_info("Creating service key file")
+
+  gar_service_provision(
+        accountId,
+        roles = roles,
+        json = json,
+        file = file,
+        email = email)
+
+  cli_alert_success("Move {file} to a secure folder location outside of your working directory")
+  moved <- usethis::ui_yeah("Have you moved the file?")
+  if(!moved){
+    cli_alert_danger("Beware! Authentication files can be used to compromise your GCP account. Do not check into git or otherwise share the file")
+    return(FALSE)
+  }
+
+  cli_alert_success("Service JSON key is now provisioned")
+  cli_alert_info("Now set up GCE_AUTH_FILE to point at file via cr_setup()")
+
+  TRUE
+
+}
+
+validate_json <- function(json){
+  validated <- tryCatch(jsonlite::fromJSON(json),
+      error = function(err){
+        cli::cli_alert_danger("Could not load alleged Client ID file: {err$message}")
+        return(FALSE)
+      })
+  if(!is.null(validated$installed$client_id)){
+    cli::cli_alert_success(
+      "Validated Client ID file {json} for project: {validated$installed$project_id}")
+    return(TRUE)
+  } else {
+    cli::cli_alert_danger("Could not read details from client ID file - is it the right one?")
+    return(FALSE)
+  }
+
+}
 
