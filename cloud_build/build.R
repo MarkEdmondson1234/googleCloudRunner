981a7deea960aad2a0b683df649230cde235eaed
Mark Edmondson <github@markedmondson.me>
2020-06-01 14:58:06 +0200

    kamiko build of googleCloudRunner docker

---
+++ cloud_build/build.R
@@ -0,0 +1,11 @@
+library(googleCloudRunner)
+
+
+docker_steps <- cr_build_yaml(
+  cr_buildstep_docker("gcr.io/gcer-public/googlecloudrunner",
+                      tag = c("latest","$BRANCH_NAME"),
+                      dockerfile = "cloud_build/Dockerfile",
+                      kaniko_cache = TRUE)
+)
+
+cr_build_write(docker_steps, file = "cloud_build/cloudbuild.yml")
