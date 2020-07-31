a0818f767f1172cdf067d4358eaf00f186d86734
Mark Edmondson <github@markedmondson.me>
2020-07-31 16:21:31 +0200

    talking people

--- inst/shiny/image_talker/app/app.R
+++ inst/shiny/image_talker/app/app.R
@@ -2,16 +2,28 @@ library(shiny)
 library(googleAuthR)
 library(googleLanguageR)
 library(googleCloudVisionR)
+library(imager)
 
 ui <- fluidPage(
   helpText("Upload an image, have it spoken back to you"),
-  selectInput("input_choice", "Where to fetch the image?",
-              choices = c("Upload","URL")),
+  radioButtons("input_choice", "Where is the image?",
+              choices = c("Upload","URL"), inline = TRUE),
+  selectInput("voice", "What voice do you want?",
+              choices = c(`Alison (GB)` = "en-GB-Wavenet-F",
+                          `Sofia (DK)` = "da-DK-Wavenet-A",
+                          `Frederik (DK)` = "da-DK-Wavenet-C",
+                          `Lise (DK)` = "da-DK-Wavenet-D",
+                          `Bruce (AU)` = "en-AU-Wavenet-B",
+                          `Shelia (AU)`= "en-AU-Wavenet-A",
+                          `Sue (GB)` = "en-GB-Wavenet-A",
+                          `Simon (GB)` = "en-GB-Wavenet-B",
+                          `Tiffany (GB)` = "en-GB-Wavenet-C",
+                          `Nigel (GB)` = "en-GB-Wavenet-D")),
   uiOutput("input_ui"),
-  actionButton("do_image", "Get Image"),
+  actionButton("do_image", "Get/Change Image"),
   uiOutput("show_image"),
+  uiOutput("image_labels"),
   h2(textOutput("big_text")),
-
   gl_talk_shinyUI("image_talk"),
   br(),
   tags$hr(),
@@ -35,6 +47,9 @@ server <- function(input, output, session){
   image_source <- eventReactive(input$do_image, {
     if(input$input_choice == "Upload"){
       message("An uploaded image")
+
+      if(is.null(input$image_input$datapath)) return(NULL)
+
       # copy to www folder
       src <-  tempfile("img","www")
       file.copy(input$image_input$datapath, src)
@@ -53,7 +68,8 @@ server <- function(input, output, session){
   output$show_image <- renderUI({
     req(image_source())
 
-    message(image_source())
+
+    message("image_source(): ", image_source())
     if(input$input_choice == "Upload"){
       src <- gsub("www/","",image_source())
     } else {
@@ -62,16 +78,19 @@ server <- function(input, output, session){
 
     tagList(
       tags$img(src=src, width="500", height="500"),
-      actionButton("do_apis", "Call APIs")
+      textInput("override_label","Override image label" ,
+                placeholder = "Provide your own image label to talk"),
+      actionButton("do_apis", "Speak this image"),
       )
 
   })
 
 
-  annotations <- eventReactive(input$do_apis, {
+  annotations <- reactive({
     req(image_source())
 
     message("upload:", image_source())
+    message("Calling vision API")
     gcv_get_image_annotations(
       image_source(),
       feature = "LABEL_DETECTION",
@@ -80,15 +99,24 @@ server <- function(input, output, session){
 
   })
 
-  output$image_text <- renderTable({
+  output$image_labels <- renderUI({
     req(annotations())
 
-    annotations()[, c("description","score", "topicality")]
+    choices <- annotations()$description
+
+    if(!is.null(input$override_label) && input$override_label != ""){
+      message("User provided label")
+      choices <- c(input$override_label, choices)
+    }
+
+    radioButtons("say_me", "Choose what to say" ,
+                 choices = choices)
   })
 
-  talk_me <- reactive({
-    req(annotations())
-    annotations()$description[[1]]
+  talk_me <- eventReactive(input$do_apis, {
+    req(input$say_me)
+    message("The voice:", input$voice)
+    input$say_me
   })
 
   output$big_text <- renderText({
@@ -97,7 +125,7 @@ server <- function(input, output, session){
   })
 
   callModule(gl_talk_shiny, "image_talk",
-             transcript = talk_me)
+             transcript = talk_me, name = input$voice)
 
 }
 
