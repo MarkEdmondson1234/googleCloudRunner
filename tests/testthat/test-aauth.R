2004da9283c008537a0f30661e3c95f0f15de3f4
Mark Edmondson <github@markedmondson.me>
2021-02-07 21:04:13 +0100

    add tests for auth

---
+++ tests/testthat/test-aauth.R
@@ -0,0 +1,13 @@
+test_that("Authentication and setup", {
+
+  expect_true(nzchar(Sys.getenv("GCE_AUTH_FILE")))
+  expect_true(nzchar(Sys.getenv("CR_REGION")))
+  expect_true(nzchar(Sys.getenv("CR_BUILD_EMAIL")))
+  expect_true(nzchar(Sys.getenv("GCS_DEFAULT_BUCKET")))
+  expect_true(nzchar(Sys.getenv("GCE_DEFAULT_PROJECT_ID")))
+
+  expect_true(
+    inherits(googleAuthR::gar_token()$auth_token, "TokenServiceAccount")
+  )
+
+})
