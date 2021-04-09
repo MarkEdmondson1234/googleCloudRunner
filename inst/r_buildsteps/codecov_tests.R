b4a2b1f3062f7f57ca4b0205f58dfa0342b17212
Mark Edmondson <github@markedmondson.me>
2021-04-09 16:55:57 +0200

    cache breaker

--- inst/r_buildsteps/codecov_tests.R
+++ inst/r_buildsteps/codecov_tests.R
@@ -1,6 +1,8 @@
 remotes::install_deps(dependencies = TRUE)
 remotes::install_local()
 cv <- covr::package_coverage()
-covr::codecov(coverage=cv,
+up <- covr::codecov(coverage=cv,
               commit = '$COMMIT_SHA', branch = '$BRANCH_NAME',
               quiet = FALSE)
+up
+if(!up$uploaded) stop("Error uploading codecov reports")
