3effcee84f94926e46f93a32f6bbbee408a4d96c
Mark Edmondson <github@markedmondson.me>
2021-09-20 08:01:03 +0200

    add build of Cloud Run regions #123

---
+++ R/cr_regions.R
@@ -0,0 +1,4 @@
+#' Cloud Run Regions
+#'
+#' @format A character vector of valid Cloud Run region names
+"cr_regions"
--- cloud_build/build.R
+++ cloud_build/build.R
@@ -2,10 +2,19 @@ library(googleCloudRunner)
 
 
 docker_steps <- cr_build_yaml(
+  c(cr_buildstep_gcloud(
+    args = c("gcloud run regions list > regions.txt"),
+    dir = "data-raw"
+  ),
+  cr_buildstep_r(
+    "data-raw/cloudrun-regions.R",
+    name = "tidyverse",
+    r_source = "runtime"
+  ),
   cr_buildstep_docker("gcr.io/gcer-public/googlecloudrunner",
                       tag = c("latest","$BRANCH_NAME"),
                       dockerfile = "cloud_build/Dockerfile",
-                      kaniko_cache = TRUE)
+                      kaniko_cache = TRUE))
 )
 
 cr_build_write(docker_steps, file = "cloud_build/cloudbuild.yml")
