351cb0a7aa652f955b557aece58d7a17d2af6298
Mark Edmondson <github@markedmondson.me>
2021-09-20 10:08:41 +0200

    move cloud run regions to the website build #123

--- cloud_build/build.R
+++ cloud_build/build.R
@@ -2,22 +2,10 @@ library(googleCloudRunner)
 
 
 docker_steps <- cr_build_yaml(
-  c(cr_buildstep_gcloud(
-    args = c(
-      "bash",
-      "-c",
-      "ls -al && gcloud run regions list > data-raw/regions.txt"),
-  ),
-  cr_buildstep_r(
-    "data-raw/cloudrun-regions.R",
-    name = "tidyverse",
-    r_source = "runtime"
-  ),
   cr_buildstep_docker("gcr.io/gcer-public/googlecloudrunner",
                       tag = c("latest","$BRANCH_NAME"),
                       dockerfile = "cloud_build/Dockerfile",
                       kaniko_cache = TRUE)
   )
-)
 
 cr_build_write(docker_steps, file = "cloud_build/cloudbuild.yml")
