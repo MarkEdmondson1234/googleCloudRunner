3b46176331770139a63b2b7da6bf8d1a31463ba9
Mark Edmondson <github@markedmondson.me>
2021-09-20 08:10:56 +0200

    where is the file

--- cloud_build/build.R
+++ cloud_build/build.R
@@ -3,7 +3,7 @@ library(googleCloudRunner)
 
 docker_steps <- cr_build_yaml(
   c(cr_buildstep_gcloud(
-    args = c("gcloud run regions list > /workspace/data-raw/regions.txt"),
+    args = c("ls -al && gcloud run regions list > data-raw/regions.txt"),
   ),
   cr_buildstep_r(
     "data-raw/cloudrun-regions.R",
