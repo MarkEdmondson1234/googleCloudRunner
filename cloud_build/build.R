e3ab4f81ee3fba600096860886446a185de0a4ee
Mark Edmondson <github@markedmondson.me>
2021-11-29 10:25:33 +0100

    timeout, vignettes

--- cloud_build/build.R
+++ cloud_build/build.R
@@ -5,7 +5,8 @@ docker_steps <- cr_build_yaml(
   cr_buildstep_docker("gcr.io/gcer-public/googlecloudrunner",
                       tag = c("latest","$BRANCH_NAME"),
                       dockerfile = "cloud_build/Dockerfile",
-                      kaniko_cache = TRUE)
+                      kaniko_cache = TRUE),
+  timeout = 3600
   )
 
 cr_build_write(docker_steps, file = "cloud_build/cloudbuild.yml")
