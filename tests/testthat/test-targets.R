3b46d5951d76e4774e290af38ac1a8e89d75a0d7
Mark Edmondson <github@markedmondson.me>
2021-12-20 18:25:17 +0100

    creating DAGs

--- tests/testthat/test-targets.R
+++ tests/testthat/test-targets.R
@@ -116,3 +116,120 @@ test_that("targets integrations", {
 
 
 })
+
+test_that("targets integrations - parallel builds", {
+  skip_on_ci()
+  skip_on_cran()
+
+  if (!require(targets)) {
+    skip("library(targets) not installed")
+  }
+
+  dir.create("targets", showWarnings = FALSE)
+  on.exit(unlink("targets", recursive = TRUE), add = TRUE)
+
+  target_folder <- "cr_build_target_tests"
+
+  target_yaml <- cr_build_targets(
+    path = NULL,
+    target_folder = target_folder,
+    tar_make = c(
+      "list.files(recursive=TRUE)",
+      "targets::tar_make(script = 'targets/_targets.R')"
+    )
+  )
+  expect_snapshot(target_yaml)
+
+  targets::tar_config_set(
+    script = "targets/_targets.R"
+  )
+
+  # test file
+  write.csv(mtcars, file = "targets/mtcars.csv", row.names = FALSE)
+
+  targets::tar_script(
+    list(
+      targets::tar_target(file1, "targets/mtcars.csv", format = "file"),
+      targets::tar_target(input1, read.csv(file1)),
+      targets::tar_target(result1, sum(input1$mpg)),
+      targets::tar_target(result2, mean(input1$mpg)),
+      targets::tar_target(result3, max(input1$mpg)),
+      targets::tar_target(result4, min(input1$mpg)),
+      targets::tar_target(merge1, paste(result1, result2, result3, result4))
+    ),
+    ask = FALSE
+  )
+
+  targets::tar_make()
+
+  # get local result to compare
+  result <- targets::tar_read("merge1")
+  expect_snapshot(result)
+
+  upload_test_files <- function() {
+    cr_build_upload_gcs(
+      "targets",
+      remote = "cr_build_target_test_source.tar.gz",
+      deploy_folder = "targets"
+    )
+  }
+
+  target_source <- upload_test_files()
+  expect_snapshot(target_source)
+
+  test_bucket <- sprintf("gs://%s/%s", cr_bucket_get(), target_folder)
+
+  bs <- cr_build_targets_dag(test_bucket)
+  expect_snapshot(bs)
+
+  par_yaml <- cr_build_targets(
+    path = NULL,
+    target_folder = target_folder,
+    create_dag = TRUE
+  )
+  expect_snapshot(par_yaml)
+
+  build <- cr_build_make(par_yaml, source = target_source)
+  # initial run
+  bb1 <- cr_build(build, launch_browser = FALSE)
+  built1 <- cr_build_wait(bb1)
+  bb1logs <- cr_build_logs(built1)
+
+  target_logs <- function(log) {
+    log[which(grepl("target pipeline", log))]
+  }
+
+  logs_of_interest1 <- target_logs(bb1logs)
+  expect_true(
+    any(
+      grepl(
+        "start target file1",
+        logs_of_interest1
+      ))
+  )
+
+  targets::tar_config_set(
+    store = "_targets_cloudbuild/cr_build_target_tests/_targets")
+  artifact_download <- cr_build_targets_artifacts(built3)
+
+  expect_true(result != targets::tar_read("merge1"))
+
+  # clean up - delete source for next test run
+  googleCloudStorageR::gcs_delete_object("cr_build_target_test_source.tar.gz",
+                                         bucket = cr_bucket_get()
+  )
+  deletes <- googleCloudStorageR::gcs_list_objects(
+    prefix = "cr_build_target_tests",
+    bucket = cr_bucket_get()
+  )
+  done_deeds <- lapply(deletes$name,
+                       googleCloudStorageR::gcs_delete_object,
+                       bucket = cr_bucket_get()
+  )
+  expect_true(all(unlist(done_deeds)))
+
+  targets::tar_destroy(ask = FALSE)
+
+  unlink("_targets.yaml")
+  unlink("_targets_cloudbuild", recursive = TRUE)
+})
