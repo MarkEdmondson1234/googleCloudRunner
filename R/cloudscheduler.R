d9538abf3aee281cb8cfa703a8ab5a54822389a8
Mark Edmondson <github@markedmondson.me>
2022-01-10 16:25:08 +0100

    add pubsub admin to roles to scheduler works

--- R/cloudscheduler.R
+++ R/cloudscheduler.R
@@ -279,8 +279,8 @@ cr_schedule_delete <- function(x,
       cr_buildtrigger_delete(the_buildtrigger$id, projectId = projectId)
 
       the_pubsub <- tryCatch({
+        # it deletes subscriptions too
         topics_delete(the_buildtrigger$pubsubConfig$topic)
-        subscriptions_delete(the_buildtrigger$pubsubConfig$subscription)
       }, error = function(err){
         myMessage("Could not delete topic and/or subscription for ",
                   the_name, "to delete. Aborting. ", err$message, level = 3)
