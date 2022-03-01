960a456964e86a99c7bb193fc0dc06855fc3024a
John Muschelli <muschellij2@gmail.com>
2022-03-01 21:58:11 +0000

    added trigger list

--- R/buildtriggers.R
+++ R/buildtriggers.R
@@ -112,6 +112,8 @@ cr_buildtrigger_delete <- function(triggerId, projectId = cr_project_get()) {
 #'
 #' @family BuildTrigger functions
 #' @param projectId ID of the project for which to list BuildTriggers
+#' @param full Return the trigger list with the build information from
+#' \code{\link{cr_buildtrigger_get}}
 #' @importFrom googleAuthR gar_api_generator
 #' @export
 #' @seealso \link{cr_build_list} which merges with this list
@@ -120,7 +122,8 @@ cr_buildtrigger_delete <- function(triggerId, projectId = cr_project_get()) {
 #'
 #' cr_buildtrigger_list()
 #' }
-cr_buildtrigger_list <- function(projectId = cr_project_get()) {
+cr_buildtrigger_list <- function(projectId = cr_project_get(),
+                                 full = FALSE) {
   url <- sprintf(
     "https://cloudbuild.googleapis.com/v1/projects/%s/triggers",
     projectId
@@ -142,17 +145,18 @@ cr_buildtrigger_list <- function(projectId = cr_project_get()) {
 
   parse_files <- function(x) {
     if (is.null(bts_df[[x]])) {
-      return(NA)
+      return(rep(NA, length = nrow(bts_df)))
     }
     unlist(lapply(bts_df[[x]], paste, collapse = ", "))
   }
 
-  data.frame(
+  df <- data.frame(
     stringsAsFactors = FALSE,
     buildTriggerName = bts_df$name,
     buildTriggerId = bts_df$id,
     buildTriggerCreateTime = bts_df$createTime,
     filename = bts_df$filename,
+    filename = if (is.null(bts_df$filename)) rep(NA, nrow(bts_df)) else bts_df$filename,
     description = bts_df$description,
     github_name = paste0(bts_df$github$owner, "/", bts_df$github$name),
     ignoredFiles = parse_files("ignoredFiles"),
@@ -160,6 +164,18 @@ cr_buildtrigger_list <- function(projectId = cr_project_get()) {
     tags = parse_files("tags"),
     disabled = if (!is.null(bts_df$disabled)) bts_df$disabled else NA
   )
+  if (full) {
+    if (NROW(df) == 0 || length(df) == 0) return(NULL)
+    trigger_data <- lapply(df$buildTriggerId, function(triggerId) {
+      x <- cr_buildtrigger_get(triggerId = triggerId,
+                               projectId = projectId)
+      data.frame(buildTriggerId = triggerId, build = I(list(x)),
+                 stringsAsFactors = FALSE)
+    })
+    trigger_data <- do.call(rbind, trigger_data)
+    df <- merge(df, trigger_data, all = TRUE, sort = FALSE)
+  }
+  df
 }
 
 parse_buildtrigger_list <- function(x) {
